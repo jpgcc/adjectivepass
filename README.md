77c0dbd5f4eff556b11ea78771753a8bc4c6d34a
João Craveiro <jpgccraveiro@gmail.com>
2017-08-08 16:01:37 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Adjective-based passphrase generator
+
+Passphrase generator. The distinguishing feature is the use of the recommended adjective order in English to produce an expression that sounds real (although it might not always make sense). By default, it uses 3 adjectives (each from one of 7 possible categories of adjectives --- 8 in the future). This produces expressions such as
+
+* `good huge green rabbits`
+* `twenty good italian computers`
+* `ancient straight golden boxes`
+* `ugly narrow curved sofas` (I actually had one of those, once)
+
+which you can use to create passwords/passphrases (using your own personal flavour of capitalisation, spacing, etc.).
+
+This is not supposed to be much feature rich, but you can definitely contribute by adding to the lists. Some contribution guidelines:
+
+* the adjectives in list 1 (relative to quantity) must make sense in the plural (that's why the numbers start at two);
+* consequently, the nouns in the nouns lists must be in their plural form.
---
+++ passgen.py
@@ -0,0 +1,36 @@
+import random
+
+number_of_adjective_lists = 7
+number_of_adjectives = 3
+
+prev = 0
+
+# list
+# 1: quantity or number. only adjective that lead to a plural noun
+# 2: Quality or opinion
+# 3: Size
+# 4: Age
+# 5: Shape
+# 6: Color
+# 7: Proper adjective (often nationality, other place of origin, or material)
+# 8 (todo): Purpose or qualifier
+# nouns: plural nouns
+
+expression = []
+
+for i in range(1, number_of_adjectives+1):
+  list_number = random.randint(prev+1, number_of_adjective_lists - (number_of_adjectives - i))
+  prev = list_number
+  with open('lists/' + str(list_number) + '.txt') as f:
+    lines = f.readlines() # todo: optimize memory usage
+  lines = [x.strip() for x in lines] 
+  line_number = random.randint(0, len(lines) - 1)
+  expression.append(lines[line_number])
+
+with open('lists/nouns.txt') as f:
+  lines = f.readlines() # todo: optimize memory usage
+lines = [x.strip() for x in lines] 
+line_number = random.randint(0, len(lines) - 1)
+expression.append(lines[line_number])
+
+print(' '.join(expression))
